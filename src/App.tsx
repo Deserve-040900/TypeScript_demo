b8bedccc4a7bb197d48abb7119e123b86f926d2b
Nguyễn Thị Hà Xuân <68209819+Deserve-040900@users.noreply.github.com>
2022-05-17 16:16:43 +0700

    MaterialUIPickers demo

--- src/App.tsx
+++ src/App.tsx
@@ -2,34 +2,11 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-class Greeting {
-  greet(): void {
-    console.log("Hello world !!!");
-  }
-}
-var obj = new Greeting();
-console.log(obj.greet());
+import MaterialUIPickers from './Components/datetimePicker';
 
 function App() {
-  var mess:string = "Hello guys !";
-  console.log(mess);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <MaterialUIPickers />
   );
 }
 
