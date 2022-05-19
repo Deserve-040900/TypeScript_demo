af6dee8dc3d6be3823fdfb8f87dfcce9b492b591
Nguyễn Thị Hà Xuân <68209819+Deserve-040900@users.noreply.github.com>
2022-05-19 08:33:48 +0700

    What did you miss ?

--- src/Components/POST_request.tsx
+++ src/Components/POST_request.tsx
@@ -1,29 +1,72 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import PostList from './postList';
 
 function PostRequest() {
-    const data = {
-                    "LANGUAGEID": "vi-VN",
-                    "MA_CTY": "DHS",
-                    "PARENTID": "01.04.00"
-                };
+    const [listJ, setListJ] = useState([]);
 
-    fetch('https://api.dhsoft.com.vn/api/HomePage/GetListDetailPage', {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify(data),
-    })
-    .then(response => response.json())
-    .then(res => {
-        console.log('Successfully', res);
-    })
-    .catch((error) => {
-        console.error('Error', error);
-        
-    });
+    useEffect(() => {
+        const data = {
+            "LANGUAGEID": "vi-VN",
+            "MA_CTY": "DHS",
+            "PARENTID": "01.04.00"
+        };
+
+        async function fetchPost() {
+            fetch('https://api.dhsoft.com.vn/api/HomePage/GetListDetailPage', {
+                method: 'POST',
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify(data),
+            })
+                .then(response => response.json())
+                .then(res => setListJ(res))
+                .catch((err) => {
+                    console.error('Error', err);
+                });
+        }
+
+        fetchPost();
+    }, []);
+
+    const post: any = { listJ }
 
     return (
-        <h1>hello guys</h1>
-    )
+        <div>
+            <div className="w3-responsive">
+                <table className="table grid text-secondary bg-light w3-border-gray">
+                    <tbody>
+                        <tr className="text-dark">
+                            <th>DETAILID</th>
+                            <th>TYPEID</th>
+                            <th>TITLE</th>
+                            <th>TITLECOLOR</th>
+                            <th>CONTENT</th>
+                            <th>CONTENTCOLOR</th>
+                            <th>SHOWBTN</th>
+                            <th>BTNCOLOR</th>
+                            <th>BTNTEXT</th>
+                            <th>BTNLINK</th>
+                            <th>IMGURL</th>
+                            <th>TAG</th>
+                            <th>POSITIONCONTENT</th>
+                            <th>IMGTYPE</th>
+                            <th>LOGOURL</th>
+                            <th>LANGUAGEID</th>
+                            <th>VIDEOURL</th>
+                            <th>ROWEXPAND</th>
+                            <th>ROWLEVEL</th>
+                            <th>CDATE</th>
+                        </tr>
+                        {post.map((p: any) => (
+                            <PostList
+                                key={p.PARENTID}
+                                p={p}
+                            />
+                        ))}
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    );
 }
 
 export { PostRequest };
