42b8de10c9e9825e4dbf1d33c8c84613bcf3a980
Nguyễn Thị Hà Xuân <68209819+Deserve-040900@users.noreply.github.com>
2022-05-19 13:21:35 +0700

    ScrollPlayground Material Demo

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,13 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-import { PostRequest } from './Components/POST_request';
+import ScrollPlayground from './Components/ScrollDemo';
 
 function App() {
   return (
     <div>
         <h3 className="p-3 text-center">Hello Guys I'm a TypeScript</h3>
-        <PostRequest />
+        <ScrollPlayground />
     </div>
   );
 }
